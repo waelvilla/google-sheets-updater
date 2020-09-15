250ddf62c92eba96ec0d1a130b38f4429f6395e8
waelvilla <waelvilla@gmail.com>
2020-09-15 19:09:35 +0300

    added matching old and new row for update

--- update.js
+++ update.js
@@ -7,9 +7,31 @@ dotenv.config()
 
 const { spreadsheetId } = process.env
 
-const makeRows = (json) => {
+const makeRow = (json = {}) => {
   const result = []
-  result.push(Object.values(json))
+  const titles = [ 'id', 'patientName', 'phone', 'category', 'selectedRole', 'selectedDisease', 'clickedTopic', 'clickedService', 'paid' ]
+  for(const key of titles){
+    if(json[key]){
+      result.push(json[key])
+    } else {
+      result.push('')
+    }
+  }
+  return result
+}
+
+const makeUpdateRows = (json, old=[]) => {
+  const values = makeRow(json)
+  const result = values.map((item, index) => {
+    if(old[index] == item){
+      return item
+    } else if(item && !old[index]){
+      return item
+    } else if(!item && old[index]){
+      return old[index]
+    }
+    return old[index]
+  })
   return result
 }
 
@@ -19,18 +41,18 @@ function run() {
   auth((client) => {
     const sheets = google.sheets({ version: 'v4', auth: client });
 
-    const rows = makeRows(data)
-    getRowId(sheets, data.id)
-      .then(id => {
-        console.log('got id: ', id);
-        updateRowById({ sheets, rowId: `A${id}`, rows })
+    getRowById(sheets, data.id)
+    .then(({row, rowId}) => {
+      console.log('got id: ', rowId);
+      const rows = makeUpdateRows(data, row)
+        updateRowById({ sheets, rowId: `A${rowId}`, rows })
       }).catch(err => console.log('error ', err))
   })
 }
 
 run()
 
-function getRowId(sheets, id) {
+function getRowById(sheets, id) {
   const column = 'A'
   return new Promise((resolve, reject) => {
     sheets.spreadsheets.values.get({
@@ -45,7 +67,7 @@ function getRowId(sheets, id) {
             rowId += 1
             if (row[0] == id) {
               console.log('found rowId', rowId);
-              resolve(rowId)
+              resolve({row, rowId})
               break;
             }
           }
